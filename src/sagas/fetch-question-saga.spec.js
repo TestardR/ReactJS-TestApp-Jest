4a45365d72fef7a831e3fe08810b3de09f7d8958
Romain <rmtestard@gmail.com>
2018-12-03 09:51:44 +0100

    mocking

--- src/sagas/fetch-question-saga.spec.js
+++ src/sagas/fetch-question-saga.spec.js
@@ -1,10 +1,16 @@
 import { handleFetchQuestion } from './fetch-question-saga';
+import fetch from 'isomorphic-fetch';
 
 describe('Fetch questions saga', () => {
+  beforeAll(() => {
+    fetch.__setValue([{ question_id: 42 }]);
+  });
   it('should fetch the questions', async () => {
     // new instance of the function generator
     const gen = handleFetchQuestion({ question_id: 42 });
     // to make it run :
     const { value } = await gen.next();
+    expect(value).toEqual([{ question_id: 42 }]);
+    expect(fetch).toHaveBeenCalledWith(`/api/questions/42`);
   });
 });
